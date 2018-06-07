8b92098739cc2aaeb49bbb7bebaaa74548499428
Frederico Alexandre <fred.bralvalex@gmail.com>
2018-06-07 12:04:01 -0400

    megha background
    megha background

---
+++ WEAPONHUNT/Assets/Camera_Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour {
+
+    [SerializeField]
+    private float xMax;
+
+    [SerializeField]
+    private float yMax;
+
+    [SerializeField]
+    private float xMin;
+
+    [SerializeField]
+    private float yMin;
+
+    private GameObject player;
+
+    // Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+	}
+}
