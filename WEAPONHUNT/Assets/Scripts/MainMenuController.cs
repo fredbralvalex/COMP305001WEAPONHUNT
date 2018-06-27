5b53e7298de3d2bd37bab7ebd70f85ab6942682b
gillkashdeep <gillkashdeep@gmail.com>
2018-06-27 12:20:56 -0400

    adjusting menus and story scenes
    adjusting menus and story scenes

--- WEAPONHUNT/Assets/Scripts/MainMenuController.cs
+++ WEAPONHUNT/Assets/Scripts/MainMenuController.cs
@@ -10,10 +10,10 @@ public class MainMenuController : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
-		public void QuitGame()
+	public void QuitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
     }
-	}
+}
 
---
+++ WEAPONHUNT/Assets/Scripts/TextEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextEffect : MonoBehaviour {
+
+    public TextMeshProUGUI TextMeshPro;
+    private float time;
+    private float timeTowait = 7;
+
+    public string TextToExhibit = "";
+    //int lenghtText;
+
+	// Use this for initialization
+	void Start () {
+        TextMeshPro.text = "";
+        //lenghtText = textToExhibit.Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time >= timeTowait * Time.deltaTime)
+        {
+            if (TextToExhibit.Length > TextMeshPro.text.Length)
+            {
+                TextMeshPro.text = TextMeshPro.text + TextToExhibit[TextMeshPro.text.Length];
+            }
+            time = 0;
+        }
+	}
+
+    public void ContinueEvent()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+}
